ba50327bc956639e39fc7edcfb6d9c20a1d514f9
tt <ttwang.303@gmail.com>
2017-05-14 17:35:31 -0400

    display request and response data

--- app.js
+++ app.js
@@ -10,10 +10,35 @@ var server = http.createServer(function(req, res) {
       res.writeHead(404);
       res.end("404 not found");
     } else {
+
+      // console.log(req.url, req.method, req.httpVersion, req.headers)
+      // console.log(res.statusMessage, res.statusCode, res._header)
+
+      var reqObj = {
+        url: req.url,
+        method: req.method,
+        httpVersion: req.httpVersion,
+        headers: req.headers
+      }
+
+      var jsonReq = JSON.stringify(reqObj, null, 2);
+
+      var resObj = {
+        statusMessage: res.statusMessage,
+        statusCode: res.statusCode,
+        header: res._header
+      }
+
+      var jsonRes = JSON.stringify(resObj, null, 2);
+
+      console.log(jsonReq)
+
       res.writeHead(200, {
         "Content-Type": "text/html"
       });
-      res.end(data);
+      data = data.replace("{{req}}", jsonReq)
+      data = data.replace("{{res}}", jsonRes)
+      res.end(data)
     }
   })
 });
