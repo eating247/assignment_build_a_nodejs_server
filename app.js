98a929e93094269a1a85a2341f5388317170cdd4
tt <ttwang.303@gmail.com>
2017-05-14 17:16:59 -0400

    outputting html

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 var http = require("http");
+var fs = require("fs");
 
 var host = 'localhost';
 var port = 3000;
 
 var server = http.createServer(function(req, res) {
-  res.writeHead(200, {
-    "Content-Type": 'text/plain'
-  });
-  res.end('hello world');
+  fs.readFile('./public/index.html', 'utf8', function(err, data) {
+    if (err) {
+      res.writeHead(404);
+      res.end("404 not found");
+    } else {
+      res.writeHead(200, {
+        "Content-Type": "text/html"
+      });
+      res.end(data);
+    }
+  })
 });
 
 server.listen(port, host, function() {
